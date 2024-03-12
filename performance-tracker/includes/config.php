7baad3cc048d2265d1103b13976ee08677b0820c
Web Extension <85253281+WebExtension1@users.noreply.github.com>
2024-03-12 14:08:28 +0000

    Delete performance-tracker directory

--- performance-tracker/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_SERVER", "127.0.0.1");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "performance-tracker");
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-?>
--- performance-tracker/includes/header.php
+++
@@ -1,5 +0,0 @@
-<div class="header-container">
-    <header>
-        <img class="logo"src="images/logo.png" alt="MIROS Logo">
-    </header>
-</div>
