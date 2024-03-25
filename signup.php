c5679e0b4d9319ef83f720fde66bdeda7e0a6b23
YusufRick <yusufrick17@gmail.com>
2024-03-25 00:09:04 +0000

    chill out

--- signup.php
+++ signup.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if ($stmt->execute()) {
             echo "<div class='alert alert-success'>You are registered successfully.</div>";
-            header("Location:../signup.php");
+            header("Location: index.php");
             exit();
         } else {
             die("Error executing statement: " . $stmt->error);
