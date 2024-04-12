89eabe5d988e4d6740ca5fd94a189521a6a07447
Web Extension <robertjenner5@outlook.com>
2024-04-12 18:55:32 +0100

    Changed 24hr to 12hr with AM/PM

--- view-submission.php
+++ view-submission.php
@@ -33,12 +33,19 @@ $author = $authorQuery->fetch_object();
                 $time = $date[1];
                 $timeValues = explode(":", $time);
                 $timeOfDay = " AM";
+                
+                $hour = intval($timeValues[0]);
+                $hour = $hour % 12;
+
+                if ($hour == 0) {
+                    $hour = 12;
+                }
+                
                 if ($timeValues[0] >= 12) {
-                    $timeValues[0] = $timeValues[0] % 12;
                     $timeOfDay = " PM";
                 }
 
-                $dateTimeOutput = $dateValues[2] . "/" . $dateValues[1] . "/" . $dateValues[0] . " AT " . $time . $timeOfDay;
+                $dateTimeOutput = $dateValues[2] . "/" . $dateValues[1] . "/" . $dateValues[0] . " at " . $hour . ":" . $timeValues[1] . ":" . $timeValues[2] . $timeOfDay;
 
                 echo "
                 <h1 class='submission-title'>$submission->title</h1>
