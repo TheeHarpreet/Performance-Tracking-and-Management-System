3e0c7a577c571802612fc73af5b1f611bffebe1a
YusufRick <yusufrick17@gmail.com>
2024-04-15 15:41:56 +0100

    translated singup

--- signup.php
+++ signup.php
@@ -111,10 +111,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_POST['lang'])) {
 function translate($key) {
     $translations = array(
         "en" => array(
-            // Things
+            "Log In" => "Log In",
+            "Email" => "Email",
+            "Password" => "Password",
+            "Invalid password" => "Invalid password",
+            "User not found" => "User not found",
+            "Login" => "Login",
+            "Don't have an account?" => "Don't have an account?",
+            "Register here" => "Register here",
+            "Language" => "Language",
+            "English" => "English",
+            "BM" => "BM",
+            "All fields are required." => "All fields are required.",
+            "Email is not valid." => "Email is not valid.",
+            "Password must be at least 8 characters long." => "Password must be at least 8 characters long.",
+            "Passwords do not match." => "Passwords do not match.",
+            "Email is already in use" => "Email is already in use",
+            "You have registered successfully." => "You have registered successfully.",
+            "Sign up" => "Sign up",
+            "First name" => "First name",
+            "Surname" => "Surname",
+            "Confirm Password" => "Confirm Password",
+            "Have an account?" => "Have an account?",
+            "Login" => "Login"
         ),
         "bm" => array(
-            // Things
+            "Log In" => "Log Masuk",
+            "Email" => "Emel",
+            "Password" => "Kata Laluan",
+            "Invalid password" => "Kata Laluan tidak sah",
+            "User not found" => "Pengguna tidak dijumpai",
+            "Login" => "Log Masuk",
+            "Don't have an account?" => "Tiada akaun?",
+            "Register here" => "Daftar di sini",
+            "Language" => "Bahasa",
+            "BM" => "BM",
+            "All fields are required." => "Semua medan diperlukan.",
+            "Email is not valid." => "Emel tidak sah.",
+            "Password must be at least 8 characters long." => "Kata laluan mesti sekurang-kurangnya 8 suku kata.",
+            "Passwords do not match." => "Kata laluan tidak sepadan.",
+            "Email is already in use" => "Emel telah digunakan",
+            "You are registered successfully." => "Anda telah berdaftar dengan berjaya.",
+            "Sign up" => "Daftar",
+            "First name" => "Nama pertama",
+            "Surname" => "Nama keluarga",
+            "Confirm Password" => "Sahkan Kata Laluan",
+            "Have an account?" => "Sudah mempunyai akaun?",
+            "Login" => "Log Masuk"
         )
     );
 
