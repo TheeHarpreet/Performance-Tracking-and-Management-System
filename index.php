81de92f7bb3013b7744c7b701ed562d9d307afae
Robert <robertjenner5@me.com>
2024-04-16 14:54:13 +0100

    Added structure to superviser Work To Review

--- index.php
+++ index.php
@@ -49,6 +49,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_POST['lang'])) {
             ?>
             <?php
                 if ($user->jobRole == "Supervisor") { // Options for supervisors to view the accounts of users they supervise.
+                    $needingReviewQuery = $mysqli->query("SELECT * FROM submission, researcherssupervisor WHERE researcherssupervisor.supervisorID = $userID AND researcherssupervisor.researcherID = submission.author AND submitted = 0");
+                    if (mysqli_num_rows($needingReviewQuery) > 0) {
+                        echo "
+                        <div>
+                        <h1>Work to review</h1>";
+                        while ($submissionToReview = $needingReviewQuery->fetch_object()) {
+                            echo "<div>";
+                            echo "$submissionToReview->title";
+                            echo "</div>";
+                        }
+                        echo "</div>";
+                    }
+
                     echo "
                     <h1>" . translate("Select a researcher to view their work") . "</h1>
                     <div class='supervisor-user-selection'>
