bd7aaa8b0df7a277b02a40c93134f36ef35c76f5
Web Extension <robertjenner5@outlook.com>
2024-04-22 15:28:10 +0100

    One last thing I promise

--- includes/submission-preview-fill.php
+++ includes/submission-preview-fill.php
@@ -1,40 +1,36 @@
 <?php
     echo "<div class='submission-preview'>";
     echo "<div class='segment-container submission-preview-box'>";
-    if ($isAuthor == false) { // $isAuthor is set before the include is called. Just adds a gray indicator to the left.
-        echo "<div class='coauthor-indicator' style='background-color: gray;'>";
-        echo "</div>";
-    }
     echo "<div class='submission-preview-text'>";
     echo "<div>";
     echo "<p>$obj->title</p>";
     $query = $mysqli->query("SELECT * FROM users WHERE userID = $obj->author");
     $author = $query->fetch_object();
-    echo "<p>By $author->fname $author->lname</p>";  // NeedsTranslation
-    echo "</div>";
-    echo "</div>";
+    echo "<p>By $author->fname $author->lname</p>";
 
     $rejectedQuery = $mysqli->query("SELECT * FROM submissionreturn WHERE submissionID = $obj->submissionID ORDER BY returnDate DESC");
     if ($obj->approved > 0) {
-        $backgroundColour = "green";
+        $status = "Approved";
     } else if ($obj->submitted == 1 && $obj->approved == 0) {
-        $backgroundColour = "orange";
+        $status = "Waiting for manager approval";
     } else if ($obj->submitted == 0 && mysqli_num_rows($rejectedQuery) > 0) {
-        $backgroundColour = "red";
+        $status = "Rejected";
         $recent = $mysqli->query("SELECT * FROM submission, submissionreturn WHERE submission.submissionID = submissionreturn.submissionID AND submissionreturn.returnDate > submission.dateSubmitted AND submission.submissionID = $obj->submissionID");
         if (mysqli_num_rows($recent) == 0) {
-            $backgroundColour = "yellow";
+            $status = "Waiting for supervisor approval";
         }
     } else {
-        $backgroundColour = "yellow";
+        $status = "Waiting for supervisor approval";
     }
-    
-    echo "<div class='colour-bar' style='background-color: $backgroundColour';>"; 
+
+    echo "<p>Status: $status</p>";
+    echo "</div>";
+    echo "</div>";
     echo "</div>";
     echo "</div>";
     echo "<div>";
     echo "<form method='post'>";
-    echo "<button class='view-details' name='submission-id' value='$obj->submissionID'>View Details</button>";  // NeedsTranslation
+    echo "<button class='view-details' name='submission-id' value='$obj->submissionID'>View Details</button>";
     echo "</form>";
     echo "</div>";
     echo "</div>";
